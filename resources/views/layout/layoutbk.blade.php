e6b359b0abd92579e1ff97248c3306c2081de471
Dmitry S <pptp78ec@gmail.com>
2022-11-12 21:36:19 +0200

    deleted search in ms and bk

--- resources/views/layout/layoutbk.blade.php
+++ resources/views/layout/layoutbk.blade.php
@@ -18,10 +18,6 @@
                 {{-- <a class="nav-link" href="{{route('moviesessions.create')}}">Add new sessions</a> --}}
               </li>  
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
